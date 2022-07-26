0c3d8869796dfab5f124b3dd9cdd3366fae5b473
Jalindar Karande <46071975+jalindar0@users.noreply.github.com>
2022-07-26 07:09:16 +0530

    Time: 4 ms (72.57%), Space: 96.9 MB (39.81%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -1,51 +1,21 @@
 class Solution {
     public int partitionDisjoint(int[] nums) {
         
-       int n = nums.length;
-       int right[] = new int[n];
-       int left[]  =  new int[n]; 
+        int pi = 0; 
         
-       int max = Integer.MIN_VALUE;
-       int min = Integer.MAX_VALUE;
-        //right array
-     
-        left[0] = nums[0];
-        for(int i = 0; i < n-1 ;i++){
-          if(nums[i] > max){
-              max = nums[i];
-              
-          }
-          left[i] = max;
-          
-          
-      }
-         
-        right[n-1] = nums[n-1];
-        for(int i = n-1; i >= 0 ;i--){
-            if(nums[i] < min){
-                min = nums[i];
-                
+        int mtp = nums[0];
+           int mts =  nums[0];
+        
+        for(int i = 0; i < nums.length; i++){
+            mts = Math.max(nums[i],mts);
+            
+            if(nums[i] < mtp){
+                pi = i;
+                mtp =mts;
             }
-            right[i] = min;
-       
-      }
-          
-          
-          
-          
-      
-        
-        int ans = -1;
-       for(int i = 0; i < n-1 ;i++){
-           if(left[i] <= right[i+1]){
-               return i+1;
-               
-           }    
-           
-       } 
-        
-        return 1;
-        
-        
+            
+            
+        }
+        return pi+1;
     }
 }
