2bc4af960f87205a05edbfca374c37946a432136
Jalindar Karande <46071975+jalindar0@users.noreply.github.com>
2022-07-26 19:37:11 +0530

    Time: 3 ms (92.16%), Space: 54.7 MB (23.21%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.java
@@ -1,21 +1,21 @@
 class Solution {
     
-     public int maximumProduct(int[] nums) {
+//      public int maximumProduct(int[] nums) {
          
-         int n = nums.length;
-         if(nums.length == 3){
-             return nums[0]*nums[1]*nums[2];
-         }
+//          int n = nums.length;
+//          if(nums.length == 3){
+//              return nums[0]*nums[1]*nums[2];
+//          }
          
-         Arrays.sort(nums);
+//          Arrays.sort(nums);
          
-         return Math.max((nums[n-1]*nums[n-2]*nums[n-3]),(nums[0]*nums[1]*nums[n-1]));
-         
-         
+//          return Math.max((nums[n-1]*nums[n-2]*nums[n-3]),(nums[0]*nums[1]*nums[n-1]));
          
          
          
-     }
+         
+         
+//      }
     
     
     
@@ -24,41 +24,41 @@ class Solution {
     
     
     
-//     public int maximumProduct(int[] nums) {
+    public int maximumProduct(int[] nums) {
         
-//         int mx1 = Integer.MIN_VALUE;
-//         int mx2 = Integer.MIN_VALUE;
-//         int mx3 = Integer.MIN_VALUE;
+        int mx1 = Integer.MIN_VALUE;
+        int mx2 = Integer.MIN_VALUE;
+        int mx3 = Integer.MIN_VALUE;
       
-//             //Negative Case
-//         int mn1 = Integer.MAX_VALUE;
-//         int mn2 = Integer.MAX_VALUE;
+            //Negative Case
+        int mn1 = Integer.MAX_VALUE;
+        int mn2 = Integer.MAX_VALUE;
             
                      
-//         for (int num : nums) {
-//           // top 3 max
-//             if (num > mx1) {
-//                 mx3 = mx2;
-//                 mx2 = mx1;
-//                 mx1 = num;
-//             } else if (num > mx2) {
-//                 mx3 = mx2;
-//                 mx2 = num;
-//             } else if (num > mx3) {
-//                 mx3 = num;
-//             }
+        for (int num : nums) {
+          // top 3 max
+            if (num > mx1) {
+                mx3 = mx2;
+                mx2 = mx1;
+                mx1 = num;
+            } else if (num > mx2) {
+                mx3 = mx2;
+                mx2 = num;
+            } else if (num > mx3) {
+                mx3 = num;
+            }
 
-//             // min top 2
+            // min top 2
             
-//             if (num < mn1) {
-//                 mn2 = mn1;
-//                 mn1 = num;
-//             } else if (num < mn2) {
-//                 mn2 = num;
-//             }
-//         }
-//         return Math.max(mx1*mx2*mx3, mx1*mn1*mn2);
-//     }
+            if (num < mn1) {
+                mn2 = mn1;
+                mn1 = num;
+            } else if (num < mn2) {
+                mn2 = num;
+            }
+        }
+        return Math.max(mx1*mx2*mx3, mx1*mn1*mn2);
+    }
         
     
 }
